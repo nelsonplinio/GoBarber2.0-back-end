b585c58abcb50a072521a4caf0d1ae837f6dbfc6
Nelson Plíni <nelsonplinio94@gmail.com>
2020-05-28 11:33:53 -0300

    Adição de um template forgot_password

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-
+import path from 'path';
 import AppError from '@shared/errors/AppError';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -31,6 +31,13 @@ class SendForgotPasswordEmailService {
       user_id: user.id,
     });
 
+    const forgotPasswordTemplatePath = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -38,10 +45,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[GoBarber] Recuperação de senha',
       templateData: {
-        template: 'Olá, {{name}}: {{token}}',
+        file: forgotPasswordTemplatePath,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token?${token}`,
         },
       },
     });
--- src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailtemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailtemplateProvider.ts
@@ -1,14 +1,19 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
 import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
 
 class HandlebarsMailtemplateProvider implements IMailTemplateProvider {
   public async parse({
-    template,
+    file,
     variables,
   }: IParseMailTemplateDTO): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+    const templateFile = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
+
+    const parseTemplate = handlebars.compile(templateFile);
 
     return parseTemplate(variables);
   }
